8e9aa77fb4937f3449f9fa455e1b108be55f3e24
James Min <jmin499@gmail.com>
2020-09-30 01:09:22 +0000

    Automatically backed up by Learn

--- arrays.js
+++ arrays.js
@@ -1,15 +1,15 @@
 var chocolateBars = ['snickers', 'hundred grand', 'kitkat', 'skittles'];
 
-var addElementToBeginningOfArray = (a, e) => [e, ...a];
-
-var destructivelyAddElementToBeginningOfArray = (a, e) => {
-  a.unshift(e)
-  return a
-}
-
-var addElementToEndOfArray = (a, e) => [a..., e];
-
-var destructivelyAddElementToEndOfArray = (a, e) => {
-  a.push(e)
-  return a
-}
+// var addElementToBeginningOfArray = (a, e) => [e, ...a];
+//
+// var destructivelyAddElementToBeginningOfArray = (a, e) => {
+//   a.unshift(e)
+//   return a
+// }
+//
+// var addElementToEndOfArray = (a, e) => [a..., e];
+//
+// var destructivelyAddElementToEndOfArray = (a, e) => {
+//   a.push(e)
+//   return a
+// }
