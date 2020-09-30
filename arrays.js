082699156ea738e31ff3a5295d173d3f2abfc1a6
James Min <jmin499@gmail.com>
2020-09-30 00:54:03 +0000

    Automatically backed up by Learn

--- arrays.js
+++ arrays.js
@@ -1,10 +1,11 @@
-const chocolateBars = ['snickers', 'hundred grad', 'kitkat', 'skittles'];
+const chocolateBars = ['snickers', 'hundred grand', 'kitkat', 'skittles'];
 
 const addElementToBeginningOfArray = (arr, e) => {
   return arr.unshift(e)
 }
 
 const destructivelyAddElementToBeginningOfArray = (arr, e) => {
-  let newArr = [e, ... arr]
+  let newArr = [... arr];
+  newArr.unshift(e)
   return newArr
 }
